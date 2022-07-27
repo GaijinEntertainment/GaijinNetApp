12afa9b91299005846c97d02f3625e74c24b265a
a.nyurenberg <a.nyurenberg@gaijin.team>
2022-07-27 14:04:35 +0300

    Move server jwt fo config.h
    To make it convenient to hide or add settings.

--- GameExamples/dungeonGame/config.h
+++ GameExamples/dungeonGame/config.h
@@ -11,4 +11,14 @@ namespace config
 
   const std::string appid = "1197";
   const std::string app_id = "50017";
+
+  const std::string server_jwt =
+      "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiI1NCIsImV4cCI6MTg5NDY2NTYwMCwiaGFzaCI"
+      "6IjU1N2M5YTc4M2VlNzE5MjkzYWE1YmJiOWUwNjM3ZjYxIiwiaWF0IjoxNjUzNDg5MTIyLCJpc3MiOiIxIiw"
+      "ia2lkIjoiMzgxNDc2IiwibG5nIjoicnUiLCJuaWNrIjoiZHVuZ2VvblRlc3RCb3QiLCJzbHQiOiJha0JkT3p"
+      "aWCIsInRncyI6IjJzdGVwLGxhbmdfcnUsc3NvIiwidWlkIjoiMTMyNzc0MjgzIn0.CGctPuoL9QGn6MgDCIa"
+      "NxaxAuQI9WXNWpvnJ2C08ai843k2Chz873pI-jZ-ulbB-GAL_w611ktha_5-RIJ8TCQXNpMcVbzk7EWH5qSS"
+      "91xWBLw4YYftGMfi-CVSpOvTecmjbpJdro2YLu5CHe6vSYlywwZpd_EH-nxn0m5D-ZGvHeuKWT5eatzqUe-H"
+      "z63kWlwa3Fh-zpGOZOJGGXOOI9Y9BCc4nNlFGx5Ek6n8-ZliBNV4NHEZjpTQWn-s2alqU-V1yhAIgMSxsXDx"
+      "HoYADs73ZWBxHKzsfaW7f4xiFd3bWNcDitxFP5tQGURtn4SI0wKtJZghQZv1xhcelRY8X6A";
 }
